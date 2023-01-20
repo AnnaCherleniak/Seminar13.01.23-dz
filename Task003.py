9982ee4491f3f9f828d2f42cdacfa1a6f730cdc0
Anna <ankakras@yandex.ru>
2023-01-20 19:54:52 +0700

    Решение задачи

--- Task003.py
+++ Task003.py
@@ -2,20 +2,16 @@
 def Print(array, rows):
     for i in range(rows):
         print(*array[i])
-def Final(array, rows):
-    if ((array[0][0] == array[0][1] == array[0][2] == 'x')
-        or (array[0][0] == array[0][1] == array[0][2] == '0')
-        or (array[1][0] == array[1][1] == array[1][2] == 'x')
-        or (array[1][0] == array[1][1] == array[1][2] == '0')
-        or (array[2][0] == array[2][1] == array[2][2] == 'x')
-        or (array[2][0] == array[2][1] == array[2][2] == '0')
-        or (array[0][0] == array[1][0] == array[2][0] == 'x')
-        or (array[0][0] == array[1][0] == array[2][0] == '0')
-        or (array[0][1] == array[1][1] == array[2][1] == 'x')
-        or (array[0][1] == array[1][1] == array[2][1] == '0')
-        or (array[0][2] == array[1][2] == array[2][2] == 'x')
-        or (array[0][2] == array[1][2] == array[2][2] == '0')
-        or (array[0][0] == array[1][1] == array[2][2] == 'x')
+def Final(array, rows, columns):
+    for i in range(rows):
+        if ((array[i][0] == array[i][1] == array[i][2] == 'x')
+            or (array[i][0] == array[i][1] == array[i][2] == '0')):
+            return '1'
+    for i in range(columns):
+        if ((array[0][i] == array[1][i] == array[2][i] == 'x')
+            or (array[0][i] == array[1][i] == array[2][i] == '0')):
+            return '1'
+    if ((array[0][0] == array[1][1] == array[2][2] == 'x')
         or (array[0][0] == array[1][1] == array[2][2] == '0')):
         return '1'
 
@@ -37,15 +33,14 @@ for i in range(rows * columns):
             if player == 0:
                 game[j][k] = 'x'   
                 Print(game, rows)
-                if Final(game, rows) == '1':
+                if Final(game, rows, columns) == '1':
                     print('Поздравлюя! Игрок номер 1, Вы выйграл!!!')
                     break
                 player = 1
-
             else:
                 game[j][k] = '0'   
                 Print(game, rows)
-                if Final(game, rows) == '1':
+                if Final(game, rows, columns) == '1':
                     print('Поздравлюя! Игрок номер 2, Вы выйграл!!!')
                     break
                 player = 0
